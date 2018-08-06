2f464d157414284d892956feb0f91cd0a8db1468
Ronel Deita <ortigas.ronel@gmail.com>
2018-08-06 15:48:43 +0800

    added scss and autoprefixer

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+          <title>ECPA</title>
+        </Head>
+        <body>
+          <Main/>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import css from '../scss/style.scss'
+
+export default () => (
+  <div className={css.test}>
+    <p className={css.nice}>Hello new Ecashpay</p>
+  </div>
+)
+
